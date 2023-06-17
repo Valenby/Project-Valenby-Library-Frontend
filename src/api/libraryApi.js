89f9501db65c11111201792b1ffcbb315748257f
Valen <99224577+Valenby@users.noreply.github.com>
2023-06-16 22:40:51 -0500

    proyecto Terminado

--- src/api/libraryApi.js
+++ src/api/libraryApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const libraryApi = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    baseURL: 'https://valenbylibrary.onrender.com/api'
 })
 
 // todo: configurar intersectores
